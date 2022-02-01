2ea75a9c5b7ff8d0163e67fb96470aef1d009257
JIVitian <jivitian@gmail.com>
2022-02-01 00:43:38 -0300

    Round Counter Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import RoundCounter from "./components/RoundCounter";
+import React, { useState } from "react";
 
 function App() {
+  const [round, setRound] = useState(1);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>AutoBingo</h1>
+        <RoundCounter round={round} setRound={setRound} />
       </header>
+      <main>
+        <section></section>
+      </main>
     </div>
   );
 }
---
+++ src/components/RoundCounter.js
@@ -0,0 +1,27 @@
+const RoundCounter = ({ round, setRound }) => {
+  const decreaseRound = () => {
+    if (round > 1) {
+      setRound(round - 1);
+    }
+  };
+
+  const increaseRound = () => {
+    if (round < 10) {
+      setRound(round + 1);
+    }
+  };
+
+  return (
+    <div className="round-container">
+      <button className="ronda-btn" onClick={decreaseRound}>
+        -
+      </button>
+      <input type="number" min="1" max="10" placeholder="Ronda" value={round} />
+      <button className="ronda-btn" onClick={increaseRound}>
+        +
+      </button>
+    </div>
+  );
+};
+
+export default RoundCounter;
